4cf05f9dcabf39f26a7a6d5276626f999986c860
the[zed]sea <lwfrqnc@gmail.com>
2016-12-31 18:45:15 -0500

    Moved the code to get the list of files inside the loop so that when the list is finished a new one is generated.  This picks up any files added since the script was first run.

--- feh_changing_wallpaper.py
+++ feh_changing_wallpaper.py
@@ -9,9 +9,10 @@ user_dir = os.path.expanduser("~") + "/"
 source_dir = user_dir + "Pictures/bg/"
 
 # get a list of filenames
-imglist = os.listdir(source_dir)
 
 while(1>0):
+    imglist = os.listdir(source_dir)
+
     for filename in imglist:
         subprocess.run(["feh", "--bg-max", source_dir + filename])
         time.sleep(timeout_sec)
